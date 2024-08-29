fa18fc8199c3b18598495858b751bd28ed43eddd
jatorrey <jacatorresre@ittepic.edu.mx>
2024-08-28 21:37:48 -0700

    Update

--- Ejercicio 6.js
+++ Ejercicio 6.js
@@ -4,6 +4,6 @@ function filtrarMayoresQue(arreglo, valor) {
     return arreglo.filter(numero => numero > valor);
 }
 
-const numerosParaFiltrar = [5, 2, 9, 1, 7];
+const numerosParaFiltrar = [2, 15, 10, 9, 3];
 const valorFiltro = 4;
 console.log(`Números mayores que ${valorFiltro}:`, filtrarMayoresQue(numerosParaFiltrar, valorFiltro));
